7f93cdefb3711efebc22eb37b0d81f7b62323236
Pierre Glandon <pglandon78@gmail.com>
2021-08-07 18:43:12 +0200

    feat(backend): add camelcase django rest framework dependency

--- backend/OCloud/settings/base.py
+++ backend/OCloud/settings/base.py
@@ -131,7 +131,16 @@ PASSWORD_HASHERS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
 }
 
 SIMPLE_JWT = {
