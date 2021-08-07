69ce8ef457098ba4dbaf7a2fe3fb1fcc2f705999
Pierre Glandon <pglandon78@gmail.com>
2021-08-07 19:03:36 +0200

    feat(frontend): fixup rest routes

--- backend/AuthKey/serializers/PublicKeySerializer.py
+++ backend/AuthKey/serializers/PublicKeySerializer.py
@@ -5,5 +5,5 @@ from AuthKey.models.PublicKey import PublicKey
 class PublicKeySerializer(serializers.ModelSerializer):
     class Meta:
         model = PublicKey
-        fields = ["name", "algorithm", "expiration_date", "value", "owner"]
+        fields = ["name", "algorithm", "expiration_date", "value", "owner", "id"]
         read_only_fields = ["owner", "algorithm"]
